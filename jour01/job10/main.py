e25fb54a842bc7b0e0d644fbb213024935f1c67c
theo-baccam <theo.baccam@laplateforme.io>
2023-11-20 13:49:12 +0100

    job09

--- jour01/job10/main.py
+++ jour01/job10/main.py
@@ -6,13 +6,24 @@
 
 montant_initial = 4000
 rendement_annuel = 102
-
-print("2024:", montant_initial * rendement_annuel / 100)
+print("2024:",
+"\nMontant Initial:",montant_initial,
+"\nRendement Annuel:",rendement_annuel,
+"\nGain Annuel:", montant_initial * rendement_annuel / 100,
+"\n")
 
 montant_initial += 5000
 rendement_annuel += 2
-print("2025:", montant_initial * rendement_annuel / 100)
+print("2025:",
+"\nMontant Initial:",montant_initial,
+"\nRendement Annuel:",rendement_annuel,
+"\nGain Annuel:", montant_initial * rendement_annuel / 100,
+"\n")
 
 montant_initial *= 0.9
 rendement_annuel -= 1
-print("2026:", montant_initial * rendement_annuel / 100)
+print("2026:",
+"\nMontant Initial:",montant_initial,
+"\nRendement Annuel:",rendement_annuel,
+"\nGain Annuel:", montant_initial * rendement_annuel / 100,
+"\n")

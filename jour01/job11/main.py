9a9329ff4ae8e67574d09a319d23ec4002b724e3
theo-baccam <theo.baccam@laplateforme.io>
2023-11-20 14:01:14 +0100

    job bonus

---
+++ jour01/job11/main.py
@@ -0,0 +1,5 @@
+string_input = input("Donnez moi une chaîne de caractère: ")
+if "e" in string_input:
+    print("Cette chaîne de caractère contient la lettre e!")
+else:
+    print("Cette chaine de caractère ne contient pas la lettre e.")

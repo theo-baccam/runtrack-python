07899fa287c323753bd066041adc4c7436151055
theo-baccam <theo.baccam@laplateforme.io>
2023-11-20 11:16:13 +0100

    job06

---
+++ jour01/job06/main.py
@@ -0,0 +1,8 @@
+# Nous créons une variable qui contient un string, la différence entre une variable
+# et une constante, c'est qu'une variable est une "bôite" dont le contenu peut-être
+# changer.
+# Contrairement à une variable, on ne peut pas changer le contenu d'une constante.
+
+ma_string = "je suis une String"
+
+print(ma_string)

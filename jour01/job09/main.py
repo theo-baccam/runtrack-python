21aa4154b30c8ba333c9d4709123dfc611ad70bb
theo-baccam <theo.baccam@laplateforme.io>
2023-11-20 12:41:58 +0100

    petit refactoring et commentaires pour job09

--- jour01/job09/main.py
+++ jour01/job09/main.py
@@ -1,9 +1,13 @@
+# On utilise une variable de type dictionaire contient plusieurs attributs de
+# type différents, utile pour la gestion de données.
 produit = {
     'nom': 'produit 1',
     'prix': 2,
     'quantite': 35
 }
 
+# `\n` sert à sauter la ligne.
+# on sépare des éléments avec une virgule.
 print("Nom:", produit['nom'], 
 "\nPrix:", produit['prix'],
 "\nQuantité:", produit['quantite'])
@@ -11,14 +15,18 @@ print("Nom:", produit['nom'],
 quantite_achat=input("Quelle est la quantité que vous souhaitez achetez? ")
 print("Cela vous côutera",2 * int(quantite_achat),"€")
 print("")
-produit['quantite'] = produit['quantite'] - int(quantite_achat)
+
+# `-=` permet de soustraire une valeur, et puis l'attribuer directement.
+# Plus simple que valeur1 = valeur1 - valeur2
+produit['quantite'] -= int(quantite_achat)
 print("Suite à l'achat",
 "\nNom:", produit['nom'], 
 "\nPrix:", produit['prix'],
 "\nQuantité:", produit['quantite'])
 print("")
 
-produit['prix'] = produit['prix'] * (( 110 / 100 ))
+# `*=` est similaire à `-=`, mais pour la multiplication.
+produit['prix'] *= (( 110 / 100 ))
 print("Suite à une hausse de prix de 10%",
 "\nNom:", produit['nom'], 
 "\nPrix:", produit['prix'],

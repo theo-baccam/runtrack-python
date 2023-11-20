ffa47c49836fbb0588d694ad41cf8a065a158fd8
theo-baccam <theo.baccam@laplateforme.io>
2023-11-20 11:04:19 +0100

    job 02 et job 04

---
+++ jour01/job04/main.py
@@ -0,0 +1,7 @@
+# J'importe le module string, qui fournits des constants et fonction pour travailler
+# avec des strings.
+import string
+
+# Ensuite nous imprimons la constante `string.ascii_lowercase`, qui est une chaîne
+# caractère contenant  tous les lettres de l'alphabet en minuscule.
+print(string.ascii_lowercase)

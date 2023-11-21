f23364373eb19f321f842caa7b513c1e91bac061
theo-baccam <theo.baccam@laplateforme.io>
2023-11-21 14:31:44 +0100

    tweak commentaire job03

--- jour02/job03/main.py
+++ jour02/job03/main.py
@@ -1,6 +1,6 @@
 # Pas besoin de définir `i` dans une boucle `for`.
 
 for i in range(101):        # range spécifie qu'on va jusqu'à 100 (101 pas inclus)
-    if i in {26, 37, 88}:   # si i est égale à ses nombres...
-        continue            # revient au début de la boucle
+    if i in {26, 37, 88}:   # si i est égale à ces nombres...
+        continue            # on revient au début de la boucle
     print(i)

e052d9cb6d762e65f01865abcf76991d8570fa23
theo-baccam <theo.baccam@laplateforme.io>
2023-11-21 14:16:40 +0100

    un caractère de plus pour job07

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -4,6 +4,6 @@ import string
 alpha = string.ascii_lowercase * 10
 alpha_len = len(alpha)
 
-for i in range(0,alpha_len,1):
+for i in range(0,alpha_len + 1,1):
     alpha_step = alpha[0:i]
     print(alpha_step)

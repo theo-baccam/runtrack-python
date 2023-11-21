6b7fb4afb723834f704494a468516e3554efb7c4
theo-baccam <theo.baccam@laplateforme.io>
2023-11-21 14:39:31 +0100

    commentaire job05

--- jour02/job05/main.py
+++ jour02/job05/main.py
@@ -1,6 +1,6 @@
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print('FizzBuzz')
+    if i % 3 == 0 and i % 5 == 0:   # Modulo permet de voir si un nombre est divisible
+        print('FizzBuzz')           # par un autre.
     elif i % 3 == 0:
         print('Fizz')
     elif i % 5 == 0:

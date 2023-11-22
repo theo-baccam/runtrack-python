7c6eca9029d6fc4a0def164fd86e8f4e07f295dd
theo-baccam <theo.baccam@laplateforme.io>
2023-11-22 11:33:51 +0100

    tweak 03-03

--- jour03/job03/main.py
+++ jour03/job03/main.py
@@ -1,6 +1,10 @@
 def Add (a,b):
-    print(a + b)
+    if a % 1 != 0 or b % 1!= 0:     # Pour vérifier si les nombres sont bien entiers
+        print('nombre invalide')
+    else:
+        print(a + b)
 
 Add(1,1)
 Add(40,2)
 Add(954,238)
+Add(25.4,21.8)

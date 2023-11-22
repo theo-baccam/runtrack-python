3fab6e9fcd62889b88fadb2b11b9cb74f33071b1
theo-baccam <theo.baccam@laplateforme.io>
2023-11-22 14:17:30 +0100

    optimisation 03-09

--- jour03/job09/main.py
+++ jour03/job09/main.py
@@ -1,12 +1,12 @@
 def moyenne(a,b,c):
     moyenne_etudiant = (int(a)+int(b)+int(c))/3
-    if moyenne_etudiant < 20 and moyenne_etudiant > 15:
+    if 20 >= moyenne_etudiant >= 15:
         print("Très bon élève")
-    elif moyenne_etudiant < 14 and moyenne_etudiant > 11:
+    elif 14 >= moyenne_etudiant >= 11:
         print("Bon élève")
-    elif moyenne_etudiant < 10 and moyenne_etudiant > 8:
+    elif 10 >= moyenne_etudiant >= 8:
         print("Elève moyen")
-    elif moyenne_etudiant < 7 and moyenne_etudiant > 0:
+    elif 7 >= moyenne_etudiant >= 0:
         print("Elève devant faire des efforts")
 
 note1 = input("Donnez une première note :")

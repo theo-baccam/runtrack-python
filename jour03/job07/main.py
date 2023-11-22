a6eb089aa7e1fa13f392ca99f078154a198419f8
theo-baccam <theo.baccam@laplateforme.io>
2023-11-22 10:44:36 +0100

    jour03 job07

---
+++ jour03/job07/main.py
@@ -0,0 +1,17 @@
+def dev_type(langage):
+    if langage == "JavaScript":
+        print("tu es un développeur web")
+    elif langage == "python":
+        print("tu es un développeur IA")
+    elif langage == "java":
+        print("tu es un développeur logiciel")
+    elif langage == "reactjs":
+        print("tu es un développeur mobile")
+    else:
+        print("un jour, je serai le meilleur de développeur...")
+    
+dev_type("JavaScript")
+dev_type("python")
+dev_type("java")
+dev_type("reactjs")
+dev_type("uhhhh")

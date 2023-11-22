a744c9eed80731a83df7ed9cb82a3e3f83d7c9ac
theo-baccam <theo.baccam@laplateforme.io>
2023-11-22 14:23:30 +0100

    tweak 03-11

--- jour03/job11/main.py
+++ jour03/job11/main.py
@@ -1,8 +1,13 @@
 def time_to_text(num):
     hour = num // 60
     mins = num - (hour * 60) 
-
-    print(f"{hour}h et", f"{mins}mins")
+    
+    if hour == 0:
+        print(f"{mins} minutes")
+    elif hour == 1:
+        print(f"{hour} heure et {mins} minute")
+    else:
+        print(f"{hour} heures et {mins} minutes")
 
 time_to_text(61)
 time_to_text(160)

bd9f839cee32aab416f835000b7de934a90bd077
theo-baccam <theo.baccam@laplateforme.io>
2023-11-22 14:07:34 +0100

    tweak 03-06

--- jour03/job06/main.py
+++ jour03/job06/main.py
@@ -1,8 +1,11 @@
 def positive_negative(nombre):
     if nombre < 0:
         print("negatif")
-    else:
+    elif nombre > 0:
         print("positif")
+    else:
+        print("zéro")
 
 positive_negative(1)
 positive_negative(-1)
+positive_negative(0)

5026e82ad794e88af22c63a5baf94c32b46210a9
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 11:39:48 +0100

    04-10 optimisation boucle for

--- jour04/job10/main.py
+++ jour04/job10/main.py
@@ -1,8 +1,8 @@
 L = [ 8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91 ]
 
 sum_interval = 0
-for i in range(len(L)):
-    if 25 <= L[i] <= 90:
-        sum_interval += L[i]
+for i in L:
+    if 25 <= i <= 90:
+        sum_interval += i
 
 print(sum_interval)

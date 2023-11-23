1322a0c62041a211ddf10f51fba9ea84e3d48594
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 14:56:41 +0100

    04-12 bubble sort

--- jour04/job12/main.py
+++ jour04/job12/main.py
@@ -1,9 +1,16 @@
 liste_1 = [732, 291, 72, 7, 92]
 liste_2 = [910, 3, 270, 19, 32]
 
-def list_sorter(list_name):
-    list_name.sort()
-    print(list_name)
+def bubble_sort(arr):
+    n = 0
+    for _ in arr:
+        n += 1
 
-list_sorter(liste_1)
-list_sorter(liste_2)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    print(arr)
+
+bubble_sort(liste_1)
+bubble_sort(liste_2)

9ac477514c914cd4c114db7ee59fa03b87219dbf
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 11:36:27 +0100

    04-08 optimisation boucle for

--- jour04/job08/main.py
+++ jour04/job08/main.py
@@ -2,8 +2,8 @@ L = [ 8, 24, 27, 48, 2, 16, 9, 7, 84, 91 ]
 
 sum_pairs = 0
 
-for i in range(len(L)):
-    if L[i] % 2 == 0:
-        sum_pairs += L[i]
+for i in L:
+    if i % 2 == 0:
+        sum_pairs += i
 
 print(sum_pairs)

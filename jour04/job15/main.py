7d3bfd766e0068ce28980dd7ebff76431b8f53d0
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 15:09:40 +0100

    04-15 bubble sort

--- jour04/job15/main.py
+++ jour04/job15/main.py
@@ -1,6 +1,14 @@
 numbers = [22.4, 4.0, 16.22, 9.10, 11.00, 12.22, 14.20, 5.20, 17.50]
 
-count = 0
-for i in numbers:
-    print(int(numbers[count]))
-    count += 1
+rounded_numbers = [int(num) for num in numbers]
+
+n = 0
+for _ in rounded_numbers:
+    n += 1
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if rounded_numbers[j] > rounded_numbers[j+1]:
+            rounded_numbers[j], rounded_numbers[j+1] = rounded_numbers[j+1], rounded_numbers[j]
+
+print(rounded_numbers)

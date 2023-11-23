03d026460683e855619db57e310a1122e5843e35
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 13:38:57 +0100

    04-14 list comprehension

--- jour04/job14/main.py
+++ jour04/job14/main.py
@@ -1,27 +1,11 @@
-string = "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance"
+import string
 
 def my_long_word(num, input_string):
-    word_start = 0
-    word_end = 1
-    current_word = ""
-    current_word_length = 0
-    output = []
-    for index, char in enumerate(input_string):
-        if (char == " " or char == ", ") and current_word_length > num:
-            current_word = input_string[word_start:word_end]
-            print(current_word_length)
-            output.append(current_word)
-            word_start = index + 1
-            current_word_length = 0
-        elif char.isalpha() or char == "'":
-            word_end = index + 1
-            current_word_length += 1
-        else:
-            word_start = index + 1
-            current_word = ""
-            current_word_length = 0
-    current_word = input_string[word_start:word_end]
-    output.append(current_word)
-    print(output)
+    words = [
+        word.strip(string.punctuation) 
+        for word in input_string.split() 
+        if word.strip(string.punctuation)[num:]]
+    print(words)
 
-my_long_word(3, string)
+input_str = "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance"
+my_long_word(3, input_str)

b34f097a566f75f89fe37b6cd833a3eac15b7d08
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 10:59:46 +0100

    04-12

---
+++ jour04/job12/main.py
@@ -0,0 +1,9 @@
+liste_1 = [732, 291, 72, 7, 92]
+liste_2 = [910, 3, 270, 19, 32]
+
+def list_sorter(list_name):
+    list_name.sort()
+    print(list_name)
+
+list_sorter(liste_1)
+list_sorter(liste_2)

f6456d80559c8cdb7ba89d8d7e7a8eca9d3305e4
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 15:19:12 +0100

    04-12 commentaire

--- jour04/job12/main.py
+++ jour04/job12/main.py
@@ -1,6 +1,11 @@
 liste_1 = [732, 291, 72, 7, 92]
 liste_2 = [910, 3, 270, 19, 32]
 
+# Algorithme bubble sort
+# L'algortithme parcourt plusieurs fois la liste, et compare deux éléments adjacents
+# si un élément est plus grand que son prochain, il les changent de place
+# Ca se répête jusqu'à que la liste soit complétement en ordre croissant.
+
 def bubble_sort(arr):
     n = 0
     for _ in arr:

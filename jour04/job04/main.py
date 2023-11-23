f9c1aaf399debdf499a689fc94a487275221d03e
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 10:08:52 +0100

    04-04

---
+++ jour04/job04/main.py
@@ -0,0 +1,8 @@
+def func_fruit():
+    fruits = ["pomme", "cerise", "orange", "Melon"]
+    print(fruits)
+    fruits.remove("orange")
+    fruits.insert(2, "Mangue")
+    print(fruits)
+
+func_fruit()

26ea9e6401eadd5a68c789b3d0611e6f4716314a
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 16:15:23 +0100

    04 10 optimisation

--- jour04/job10/main.py
+++ jour04/job10/main.py
@@ -1,8 +1,11 @@
+from math import prod
+
 L = [ 8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91 ]
 
-prod_interval = 1
-for i in L:
-    if 25 <= i <= 90:
-        prod_interval *= i
+prod_interval = [
+    i for i in L if 25 <= i <= 90
+]
+
+prod_interval = prod(prod_interval)
 
 print(prod_interval)

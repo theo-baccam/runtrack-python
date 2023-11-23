21e6cbc5434a444965d8901791c6cb7e681cac71
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 14:23:28 +0100

    04-10 correction

--- jour04/job10/main.py
+++ jour04/job10/main.py
@@ -1,8 +1,8 @@
 L = [ 8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91 ]
 
-sum_interval = 0
+sum_interval = 1
 for i in L:
     if 25 <= i <= 90:
-        sum_interval += i
+        sum_interval *= i
 
 print(sum_interval)

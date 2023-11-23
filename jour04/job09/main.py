c5164c01c677e5bbbe5b8f2f739a8194b9a94d98
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 14:20:54 +0100

    04-09 modification

--- jour04/job09/main.py
+++ jour04/job09/main.py
@@ -1,6 +1,4 @@
 L = [ 8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
 
-L.sort()
-
-print(f"la valeur max est : {L[-1]}")
-print(f"la valeur min est : {L[0]}")
+print(f"la valeur max est : {max(L)}")
+print(f"la valeur min est : {min(L)}")

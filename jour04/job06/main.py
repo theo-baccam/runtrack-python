d30fa743759f5877f26916a6de194041e9265acf
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 16:05:46 +0100

    04-06 optimisation

--- jour04/job06/main.py
+++ jour04/job06/main.py
@@ -1,13 +1,7 @@
 liste = [ 1, 2, 3, 4, 5 ]
 
-first_value = liste[0]
-last_value = liste[-1]
 print(liste)
 
-liste.pop(-1)
-liste.append(first_value)
-
-liste.pop(0)
-liste.insert(0, last_value)
+liste[0], liste[-1] = liste[-1], liste[0]
 
 print(liste)

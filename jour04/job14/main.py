139dc35a51b6ee2e2a073dd6b240dc55faa47605
theo-baccam <theo.baccam@laplateforme.io>
2023-11-23 15:27:51 +0100

    04-14 commentaire

--- jour04/job14/main.py
+++ jour04/job14/main.py
@@ -1,10 +1,13 @@
 import string
 
+# Faire une list comprehension est plus conscis que faire une boucle for lorsqu'on
+# souhaite créer une liste selon des conditions.
+
 def my_long_word(num, input_string):
     words = [
-        word.strip(string.punctuation) 
-        for word in input_string.split() 
-        if word.strip(string.punctuation)[num:]]
+        word.strip(string.punctuation)              # Enlève toute ponctuations
+        for word in input_string.split()            # Sépare selon espaces
+        if word.strip(string.punctuation)[num:]]    # Si un mot est au dessus de 'num'
     print(words)
 
 input_str = "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance"

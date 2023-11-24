305d5c6d37364b2a12ceba30348fe7b8b06c27ea
theo-baccam <theo.baccam@laplateforme.io>
2023-11-24 14:01:08 +0100

    05-04 changement nom variable, commentaires

--- jour05/job04/main.py
+++ jour05/job04/main.py
@@ -1,12 +1,26 @@
 def diagonal_carpet(n):
-    x_line = "-" * (n + 1)
-    print(f"+{x_line}+")
+    # Bord pour le haut et le bas, n + 1 afin que la diagonale va correctement d'un
+    # coin à l'autre.
+    border_line = "-" * (n + 1)
+
+    # Dessiner le bord haut
+    print(f"+{border_line}+")
+
+    # Le nombre de caractère avant de laisser un espace vide
     val = n
+
+    # n + 1 car range() ne marche pas inclusivement, c'est à dire que si on met
+    # range(100), ça va aller jusqu'à 99, pas 100
     for i in range(n + 1):
-        char = "#" * val
-        empt = "#" * (n - val)
-        print(f"|{char} {empt}|")
+        left = "#" * val
+        right = "#" * (n - val)
+
+        # Dessiner le bord bas
+        print(f"|{left} {right}|")
+
+        # Réduire le nombre de caractère à dessiner pour le partie gauche.
         val -= 1
-    print(f"+{x_line}+")
+
+    print(f"+{border_line}+")
 
 diagonal_carpet(10)

492bbd5d1759cbec46436579962d755c4bb76579
theo-baccam <theo.baccam@laplateforme.io>
2023-11-24 13:54:07 +0100

    05-03 commentaires

--- jour05/job03/main.py
+++ jour05/job03/main.py
@@ -1,25 +1,29 @@
-# l_empt
-# l_diag
-# r_diag
-# r_empt
-# m_empt
-# m_line
-# middle is height*2 - 2 - empty space
-
 def triangle(height):
-    l_diag = "/"
-    r_diag = "\\"
+    l_diag = "/"        # Caractères pour côtés du triangle
+    r_diag = "\\"       # Il y a deux backslashes car '\' est un caarctère spécial
+
+    # Pour calculer la marge avant de dessiner
     empty_side = height - 1
+    # Pour calculer l'espace à l'intérieur du triangle
     empty_mid = height * 2 - empty_side * 2 - 2
+
+    # Boucle permettant de dessiner le triangle
     for i in range(height):
+        # Insérer du vide à côté et à l'intérieur du triangle
         empty_char = " " * empty_side
         empty_char_mid = " " * empty_mid
+
+        # Si on a atteint la base du triangle, changer le caractère de empty_char_mid
         if empty_mid == height * 2 - 2:
             empty_char_mid = "_" * empty_mid
-        print(f"{empty_char}{l_diag}{empty_char_mid}{r_diag}{empty_char}")
+
+        # Desisiner le triangle
+        print(f"{empty_char}{l_diag}{empty_char_mid}{r_diag}")
+
+        # Elargir l'espace à l'intérieur du triangle
         empty_side -= 1
         empty_mid += 2
 
-triangle(-2)
+triangle(5)
 
 ## Rajouter boucle si chiffre est négatif.

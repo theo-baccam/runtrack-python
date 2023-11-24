6e0eedb9409707ec0a5b0e6d34bda57053c2f661
theo-baccam <theo.baccam@laplateforme.io>
2023-11-24 13:46:01 +0100

    05-02 changement nom variable, amelioration lisibilite et commentaires

--- jour05/job02/main.py
+++ jour05/job02/main.py
@@ -1,13 +1,18 @@
 def rectangle(width, height):
-    if width < 2 or height < 2:
-        raise ValueError("Dimensiosn trop petites, au moins 2x2")
+    if width < 2 or height < 2:     # Vérification de dimensions
+        raise ValueError("Dimensions trop petites, au moins 2x2")
+
+    # Largeur ajusté pour prendre en compte les bords gauche et droite
     width -= 2
-    x_line = "-" * width
-    x_empty = " " * width
-    print(f'|{x_line}|')
-    for i in range(height - 2):
-        print(f'|{x_empty}|')
-    print(f'|{x_line}|')
+
+    width_line = "-" * width        # Ligne horizontale pour les bords haut et bas
+
+    width_empty = " " * width       # Vide à l'intérieur du rectangle
+
+    print(f'|{width_line}|')        # Afficher le bord haut
+    for i in range(height - 2):     # Afficher lignes vertical avec vides entre eux
+        print(f'|{width_empty}|')
+    print(f'|{width_line}|')        # Afficher le bord bas
 
 rectangle(2,2)
 rectangle(5,9)

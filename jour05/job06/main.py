0ef3de74c95b7351b48e8b20a7b65f739b6a2410
theo-baccam <theo.baccam@laplateforme.io>
2023-11-25 00:07:56 +0100

    05-06 correction calcul

--- jour05/job06/main.py
+++ jour05/job06/main.py
@@ -1,7 +1,5 @@
 def lighthouse(steps, step_height):
-    # Calculer le nombre de centimètre parcouru en une semaine, et puis la convertir
-    # en mètre.
-    weekly = (steps * step_height) * 2 * 7 / 100
+    weekly = (steps * step_height) * 10 * 7 / 100
 
     return (
         f"Pour marcher {steps} marches de {step_height}cm, "

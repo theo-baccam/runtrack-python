2593a0b006c44f83a2fb75252b9776e06563dc77
theo-baccam <theo.baccam@laplateforme.io>
2023-11-24 13:27:51 +0100

    05-06 amelioration lisibilité sur string

--- jour05/job06/main.py
+++ jour05/job06/main.py
@@ -1,5 +1,8 @@
 def lighthouse(steps, step_height):
     weekly = (steps * step_height) * 2 * 7 / 100
-    print(f"Pour marcher {steps} marches de {step_height}cm, le gardien parcourt {weekly}m par semaine.")
+    print(
+        f"Pour marcher {steps} marches de {step_height}cm, "
+        f"le gardien parcourt {weekly}m par semaine."
+    )
 
 lighthouse(40,7.5)

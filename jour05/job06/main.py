8c3e4fdf724291d5d800124c458c994a966f2d74
theo-baccam <theo.baccam@laplateforme.io>
2023-11-24 14:06:10 +0100

    commentaire

--- jour05/job06/main.py
+++ jour05/job06/main.py
@@ -1,5 +1,8 @@
 def lighthouse(steps, step_height):
+    # Calculer le nombre de centimètre parcouru en une semaine, et puis la convertir
+    # en mètre.
     weekly = (steps * step_height) * 2 * 7 / 100
+
     print(
         f"Pour marcher {steps} marches de {step_height}cm, "
         f"le gardien parcourt {weekly}m par semaine."

31ebf233c5b85fa8862bfd2ba5ff9d996e520715
theo-baccam <theo.baccam@laplateforme.io>
2023-11-24 14:12:59 +0100

    05-07 commentaires

--- jour05/job07/main.py
+++ jour05/job07/main.py
@@ -1,5 +1,13 @@
 def luke_notes(input_list):
     output_list = []
+
+    # On utilise une list comprehension car c'est plus court qu'une boucle for.
+    # Si un nombre dans liste est en dessous de 40, la mettre dans la liste output
+    # tel quel.
+    # Si il y un nombre à un multiple de 5 existe à moins de 3 points au dessous,
+    # arrondir la note à ça.
+    # Sinon rendre la note tel quel.
+    # Si une note est en dessous de 0 et au dessus de i, ne pas l'inclure.
     output_list = [
         i if i < 40 else i + (5 - i % 5) if i % 5 > 2 else i
         for i in input_list

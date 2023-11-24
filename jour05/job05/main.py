9395293ef6996b1fa0c215fc28eaf0ea7e5776e9
theo-baccam <theo.baccam@laplateforme.io>
2023-11-24 14:04:58 +0100

    05-05 commentaires

--- jour05/job05/main.py
+++ jour05/job05/main.py
@@ -1,13 +1,23 @@
 def true_to_caesar(input_msg,shift):
     output_msg = ""
+
     for char in input_msg:
+        # Si le caractère est une lettre de l'alphabet, faire un décalage
         if char.isalpha():
+
+            # ord() permet d'obtenir l'unicode d'un caractère
             offset = ord('A') if char.isupper() else ord('a')
+
+            # Décalage
+            # Modulo 26 permet de revenir à 0 au cas où on sort de l'alphabet.
+            # Par exemple si on est à Z est qu'on décale de 1, on va à A, et pas
+            # sur un symbole spécial.
             output_msg += chr((ord(char) - offset + shift) % 26 + offset)
         else:
+            # Si ce n'est pas une lettre de l'alphabet, pas besoin de la décaler.
             output_msg += char
+
     print(output_msg)
-        
 
 input_str = "The quick brown fox jumped over the lazy dog."
 true_to_caesar(input_str,3)
